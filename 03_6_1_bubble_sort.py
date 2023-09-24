ef26eb0363eabbe4e04750fc2354f19857f9f25b
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2023-09-24 17:27:42 +0200

    Update 03_6_1_bubble_sort.py

--- 03_6_1_bubble_sort.py
+++ 03_6_1_bubble_sort.py
@@ -1,24 +1,4 @@
-# 1 decision:
-
-nums = [int(x) for x in input().split()]
-
-is_sorted = False
-counter = 0
-while not is_sorted:
-    is_sorted = True
-    for i in range(1, len(nums) - counter):
-        if nums[i] < nums[i - 1]:
-            nums[i], nums[i - 1] = nums[i - 1], nums[i]
-            is_sorted = False
-    counter += 1
-
-print(*nums, sep=" ")
-
-
-
-
-
-# 2 decision:
+# 2 variant:
 
 nums = [int(x) for x in input().split()]
 for i in range(len(nums)):
