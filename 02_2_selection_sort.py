6fda2db89116cf22cfd1865fb84c25789c082299
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2023-09-24 16:18:05 +0200

    Update 02_2_selection_sort.py

--- 02_2_selection_sort.py
+++ 02_2_selection_sort.py
@@ -9,14 +9,4 @@ for i in range(len(numbers)):
 
 print(*numbers)
 
-# def selection_sort(numbers, sorted_nums):
-#     if not numbers:
-#         return sorted_nums
-#     min_element = min(numbers)
-#     sorted_nums.append(min_element)
-#     numbers.remove(min_element)
-#     return selection_sort(numbers, sorted_nums)
-#
-#
-# list_numbers = list(map(int, input().split()))
-# print(*selection_sort(list_numbers, []))
+
